eedf0eec0fab2bdf4453bcf697a9e55227fb7335
Maciej Bogusz <maciekbogusz@yahoo.com>
2018-10-01 13:36:33 +0200

    - added files with tests

---
+++ post_tests.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from post import Post
+
+class PostTest(TestCase):
+   def test_create_post(self): 
+       p = Post('Test', 'TestContent')
+       
+       self.assertEqual('Test', p.title)
+       self.assertEqual('TestContent', p.content)
+       
+   def test_json(self):
+       p = Post('Test', 'Test Content')
+       expected = {'title': 'Test', 'content': 'Test Content'}
+       
+       self.assertDictEqual(expected, p.json())
+       
