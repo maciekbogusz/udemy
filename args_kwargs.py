dba2b7be8f28197f4fcda5ec6b4435a641430f44
Maciej Bogusz <maciekbogusz@yahoo.com>
2018-10-01 12:38:02 +0200

    Add files via upload

---
+++ args_kwargs.py
@@ -0,0 +1,5 @@
+def my_method(arg1, arg2):
+    return arg1+arg2
+
+my_method(5,6)
+
---
+++ inheritence.py
@@ -0,0 +1,32 @@
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+        
+    def average(self):
+        return sum(self.marks/len(self.marks))
+    
+    @classmethod
+    def friend(cls, origin, friend_name, salary):
+        return cls(friend_name, origin.school, salary)
+    
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super().__init__(name, school)
+        self.salary = salary
+
+#anna = Student("Anna", "Oxford")
+
+anna = WorkingStudent("Anna", "Oxford", 22.00)
+print(anna.salary)
+friend = WorkingStudent.friend(anna, "Peter", 20)
+print(friend.name)
+print(friend.school)
+print(friend.sa)
+ 
+
+
+
+
+
